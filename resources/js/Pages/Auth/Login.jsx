60404cec6aeebcaf2e4102b4d944ff01f9acb775
Lucas Couto <lucascouto.contato@gmail.com>
2024-04-24 22:23:08 -0300

    implementação de login

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -1,97 +1,104 @@
-import { useEffect } from 'react';
-import Checkbox from '@/Components/Checkbox';
-import GuestLayout from '@/Layouts/GuestLayout';
-import InputError from '@/Components/InputError';
-import InputLabel from '@/Components/InputLabel';
-import PrimaryButton from '@/Components/PrimaryButton';
-import TextInput from '@/Components/TextInput';
-import { Head, Link, useForm } from '@inertiajs/react';
+import {useLayoutEffect, useState} from 'react';
+import * as Yup from 'yup';
+import backgroundImage from '../../assets/bg-image.png';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLock, faUser } from "@fortawesome/free-solid-svg-icons";
+import {useFormik} from "formik";
 
-export default function Login({ status, canResetPassword }) {
-    const { data, setData, post, processing, errors, reset } = useForm({
-        email: '',
-        password: '',
-        remember: false,
-    });
+function Login() {
+    const [isDesktop, setIsDesktop] = useState(false);
 
-    useEffect(() => {
-        return () => {
-            reset('password');
-        };
+    useLayoutEffect(() => {
+        function updateSize() {
+            setIsDesktop(window.innerWidth >= 1000);
+        }
+        window.addEventListener('resize', updateSize);
+        updateSize();
+        return () => window.removeEventListener('resize', updateSize);
     }, []);
 
-    const submit = (e) => {
-        e.preventDefault();
-
-        post(route('login'));
-    };
+    const formik = useFormik({
+        initialValues: {
+            username: '',
+            password: '',
+        },
+        validationSchema: Yup.object().shape({
+            username: Yup.string().required('O usuário é obrigatório'),
+            password: Yup.string().required('A senha é obrigatória'),
+        }),
+        onSubmit: values => {
+            // Aqui você pode enviar os dados do formulário para a API ou realizar outras ações
+            console.log('Valores do formulário:', values);
+        },
+    });
 
     return (
-        <GuestLayout>
-            <Head title="Log in" />
-
-            {status && <div className="mb-4 font-medium text-sm text-green-600">{status}</div>}
-
-            <form onSubmit={submit}>
-                <div>
-                    <InputLabel htmlFor="email" value="Email" />
-
-                    <TextInput
-                        id="email"
-                        type="email"
-                        name="email"
-                        value={data.email}
-                        className="mt-1 block w-full"
-                        autoComplete="username"
-                        isFocused={true}
-                        onChange={(e) => setData('email', e.target.value)}
-                    />
-
-                    <InputError message={errors.email} className="mt-2" />
+        <>
+            <div className={`bg-white grid font-interRegular h-screen overflow-hidden ${isDesktop ? 'grid-cols-3' : 'grid-cols-1'}`}>
+                <div className="bg-white">
+                    <header className='flex flex-col gap-4 justify-center items-center' style={{ height: '40%' }}>
+                        <h1 className='text-4xl font-interExtraBold'>
+                            LOGO
+                        </h1>
+                        <div className='mt-5 text-2xl font-interBold text-green-700'>
+                            <h5>Seja bem vindo!</h5>
+                        </div>
+                        <div className='text-lg font-interSemiBold text-green-700'>
+                            <h5>Faça login!</h5>
+                        </div>
+                    </header>
+                    <form onSubmit={formik.handleSubmit} className='flex flex-col gap-4 items-center h-full' style={{ height: '60%' }}>
+                        <div className='flex flex-col gap-2'>
+                            <label className='flex items-center font-montserratMedium text-xs gap-4 text-green-700'>
+                                <FontAwesomeIcon icon={faUser} />
+                                Usuário
+                            </label>
+                            <input
+                                type={"text"}
+                                placeholder={'Login'}
+                                className='border-green-700 border-2 px-8 py-2.5 rounded-xl'
+                                name="username"
+                                value={formik.values.username}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                            />
+                            {formik.touched.username && formik.errors.username ? (
+                                <div className="text-red-600 text-xs">{formik.errors.username}</div>
+                            ) : null}
+                        </div>
+                        <div className='flex flex-col gap-2'>
+                            <label className='flex items-center font-montserratMedium text-xs gap-4 text-green-700'>
+                                <FontAwesomeIcon icon={faLock} />
+                                Senha
+                            </label>
+                            <input
+                                type={"password"}
+                                placeholder={'Senha'}
+                                className='border-green-700 border-2 px-8 py-2.5 rounded-xl'
+                                name="password"
+                                value={formik.values.password}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                            />
+                            {formik.touched.password && formik.errors.password ? (
+                                <div className="text-red-600 text-xs">{formik.errors.password}</div>
+                            ) : null}
+                        </div>
+                        <div className='mt-5'>
+                            <button
+                                type="submit"
+                                className='text-white px-28 py-2.5 rounded-xl font-bold'
+                                style={{ backgroundColor: '#FEB132' }}
+                            >
+                                Entrar
+                            </button>
+                        </div>
+                    </form>
                 </div>
-
-                <div className="mt-4">
-                    <InputLabel htmlFor="password" value="Password" />
-
-                    <TextInput
-                        id="password"
-                        type="password"
-                        name="password"
-                        value={data.password}
-                        className="mt-1 block w-full"
-                        autoComplete="current-password"
-                        onChange={(e) => setData('password', e.target.value)}
-                    />
-
-                    <InputError message={errors.password} className="mt-2" />
-                </div>
-
-                <div className="block mt-4">
-                    <label className="flex items-center">
-                        <Checkbox
-                            name="remember"
-                            checked={data.remember}
-                            onChange={(e) => setData('remember', e.target.checked)}
-                        />
-                        <span className="ms-2 text-sm text-gray-600">Remember me</span>
-                    </label>
-                </div>
-
-                <div className="flex items-center justify-end mt-4">
-                    {canResetPassword && (
-                        <Link
-                            href={route('password.request')}
-                            className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                        >
-                            Forgot your password?
-                        </Link>
-                    )}
-
-                    <PrimaryButton className="ms-4" disabled={processing}>
-                        Log in
-                    </PrimaryButton>
-                </div>
-            </form>
-        </GuestLayout>
+                <div className={`col-span-2 bg-cover bg-center ${isDesktop ? '' : 'hidden'}`} style={{ backgroundImage: `url(${backgroundImage})` }}></div>
+            </div>
+        </>
     );
 }
+
+export default Login;
