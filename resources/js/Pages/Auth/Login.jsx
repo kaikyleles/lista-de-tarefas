e531e20ffe176a81b18c87d45185aabe007f1dcb
Lucas Couto <81658256+LucasCouto13@users.noreply.github.com>
2024-04-29 21:41:42 -0300

    tela de login e registro

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -1,15 +1,17 @@
 import {useLayoutEffect, useState} from 'react';
+import { useForm } from '@inertiajs/inertia-react';
 import * as Yup from 'yup';
-import backgroundImage from '../../assets/bg-image.png';
+import backgroundImage from '../../../assets/bg-image.png';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLock, faUser } from "@fortawesome/free-solid-svg-icons";
+import { faLock, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import {useFormik} from "formik";
+import { Inertia } from '@inertiajs/inertia';
 
-function Login() {
+const Login = () => {
     const [isDesktop, setIsDesktop] = useState(false);
 
     useLayoutEffect(() => {
-        function updateSize() {
+        const updateSize = () => {
             setIsDesktop(window.innerWidth >= 1000);
         }
         window.addEventListener('resize', updateSize);
@@ -19,51 +21,57 @@ function Login() {
 
     const formik = useFormik({
         initialValues: {
-            username: '',
+            email: '',
             password: '',
         },
         validationSchema: Yup.object().shape({
-            username: Yup.string().required('O usuário é obrigatório'),
-            password: Yup.string().required('A senha é obrigatória'),
+            email: Yup.string()
+                .email("O e-mail deve ser válido")
+                .required('O e-mail é obrigatório'),
+            password: Yup.string().required('A senha é obrigatória').min(6, "Mínimo de 6 caracteres"),
         }),
         onSubmit: values => {
-            // Aqui você pode enviar os dados do formulário para a API ou realizar outras ações
-            console.log('Valores do formulário:', values);
+            auth();
         },
     });
+
+    const auth = () => {
+        Inertia.visit('/');
+    }
 
     return (
         <>
             <div className={`bg-white grid font-interRegular h-screen overflow-hidden ${isDesktop ? 'grid-cols-3' : 'grid-cols-1'}`}>
                 <div className="bg-white">
                     <header className='flex flex-col gap-4 justify-center items-center' style={{ height: '40%' }}>
-                        <h1 className='text-4xl font-interExtraBold'>
+                        <h1 className='text-3xl font-interExtraBold'>
                             LOGO
                         </h1>
-                        <div className='mt-5 text-2xl font-interBold text-green-700'>
+                        <div className='mt-5 text-xl font-interBold text-green-700'>
                             <h5>Seja bem vindo!</h5>
                         </div>
-                        <div className='text-lg font-interSemiBold text-green-700'>
+                        <div className='text-md font-interSemiBold text-green-700'>
                             <h5>Faça login!</h5>
                         </div>
                     </header>
-                    <form onSubmit={formik.handleSubmit} className='flex flex-col gap-4 items-center h-full' style={{ height: '60%' }}>
+                    <form onSubmit={formik.handleSubmit} className='flex flex-col gap-4 items-center h-full'
+                          style={{height: '60%'}}>
                         <div className='flex flex-col gap-2'>
                             <label className='flex items-center font-montserratMedium text-xs gap-4 text-green-700'>
-                                <FontAwesomeIcon icon={faUser} />
-                                Usuário
+                                <FontAwesomeIcon icon={faEnvelope}/>
+                                E-mail
                             </label>
                             <input
                                 type={"text"}
-                                placeholder={'Login'}
-                                className='border-green-700 border-2 px-8 py-2.5 rounded-xl'
-                                name="username"
-                                value={formik.values.username}
+                                placeholder={'E-mail'}
+                                className='border-green-700 border-2 text-sm px-8 py-2 rounded-xl'
+                                name="email"
+                                value={formik.values.email}
                                 onChange={formik.handleChange}
                                 onBlur={formik.handleBlur}
                             />
-                            {formik.touched.username && formik.errors.username ? (
-                                <div className="text-red-600 text-xs">{formik.errors.username}</div>
+                            {formik.touched.email && formik.errors.email ? (
+                                <div className="text-red-600 text-xs">{formik.errors.email}</div>
                             ) : null}
                         </div>
                         <div className='flex flex-col gap-2'>
@@ -74,7 +82,7 @@ function Login() {
                             <input
                                 type={"password"}
                                 placeholder={'Senha'}
-                                className='border-green-700 border-2 px-8 py-2.5 rounded-xl'
+                                className='border-green-700 border-2 px-8 py-2 text-sm rounded-xl'
                                 name="password"
                                 value={formik.values.password}
                                 onChange={formik.handleChange}
@@ -87,18 +95,24 @@ function Login() {
                         <div className='mt-5'>
                             <button
                                 type="submit"
-                                className='text-white px-28 py-2.5 rounded-xl font-bold'
-                                style={{ backgroundColor: '#FEB132' }}
+                                className='text-white px-28 py-2 rounded-xl font-bold text-sm'
+                                style={{backgroundColor: '#FEB132'}}
                             >
                                 Entrar
                             </button>
                         </div>
+                        <div className='mt-3 text-xs'>
+                            <p>Não possui conta? <a href={route('register')}
+                                                    className='text-green-700 font-bold cursor-pointer'>Cadastre-se</a>
+                            </p>
+                        </div>
                     </form>
                 </div>
-                <div className={`col-span-2 bg-cover bg-center ${isDesktop ? '' : 'hidden'}`} style={{ backgroundImage: `url(${backgroundImage})` }}></div>
+                <div className={`col-span-2 bg-cover bg-center ${isDesktop ? '' : 'hidden'}`}
+                     style={{backgroundImage: `url(${backgroundImage})`}}></div>
             </div>
         </>
     );
 }
-
 export default Login;
+
--- resources/js/Pages/Auth/Register.jsx
+++ resources/js/Pages/Auth/Register.jsx
@@ -1,116 +1,127 @@
-import { useEffect } from 'react';
-import GuestLayout from '@/Layouts/GuestLayout';
-import InputError from '@/Components/InputError';
-import InputLabel from '@/Components/InputLabel';
-import PrimaryButton from '@/Components/PrimaryButton';
-import TextInput from '@/Components/TextInput';
-import { Head, Link, useForm } from '@inertiajs/react';
+import {useLayoutEffect, useState} from 'react';
+import {useFormik} from "formik";
+import * as Yup from "yup";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faEnvelope, faLock, faUser} from "@fortawesome/free-solid-svg-icons";
+import backgroundImage from "../../../assets/Design sem nome.png";
 
 export default function Register() {
-    const { data, setData, post, processing, errors, reset } = useForm({
-        name: '',
-        email: '',
-        password: '',
-        password_confirmation: '',
-    });
+    const [isDesktop, setIsDesktop] = useState(false);
 
-    useEffect(() => {
-        return () => {
-            reset('password', 'password_confirmation');
-        };
+    useLayoutEffect(() => {
+        const updateSize = () => {
+            setIsDesktop(window.innerWidth >= 1000);
+        }
+        window.addEventListener('resize', updateSize);
+        updateSize();
+        return () => window.removeEventListener('resize', updateSize);
     }, []);
 
-    const submit = (e) => {
-        e.preventDefault();
-
-        post(route('register'));
-    };
+    const formik = useFormik({
+        initialValues: {
+            username: '',
+            email: '',
+            password: '',
+        },
+        validationSchema: Yup.object().shape({
+            username: Yup.string()
+                .required('O nome é obrigatório'),
+            email: Yup.string()
+                .email("O e-mail deve ser válido")
+                .required('O e-mail é obrigatório'),
+            password: Yup.string().required('A senha é obrigatória').min(6, "Mínimo de 6 caracteres"),
+        }),
+        onSubmit: values => {
+        },
+    });
 
     return (
-        <GuestLayout>
-            <Head title="Register" />
-
-            <form onSubmit={submit}>
-                <div>
-                    <InputLabel htmlFor="name" value="Name" />
-
-                    <TextInput
-                        id="name"
-                        name="name"
-                        value={data.name}
-                        className="mt-1 block w-full"
-                        autoComplete="name"
-                        isFocused={true}
-                        onChange={(e) => setData('name', e.target.value)}
-                        required
-                    />
-
-                    <InputError message={errors.name} className="mt-2" />
+        <>
+            <div className={`bg-white grid font-interRegular h-screen overflow-hidden ${isDesktop ? 'grid-cols-3' : 'grid-cols-1'}`}>
+                <div className="bg-white">
+                    <header className='flex flex-col gap-4 justify-center items-center' style={{ height: '35%' }}>
+                        <h1 className='text-3xl font-interExtraBold'>
+                            LOGO
+                        </h1>
+                        <div className='mt-5 text-xl font-interBold text-green-700'>
+                            <h5>Faça parte, cadastre-se!</h5>
+                        </div>
+                    </header>
+                    <form onSubmit={formik.handleSubmit} className='flex flex-col gap-4 items-center h-full'
+                          style={{height: '65%'}}>
+                        <div className='flex flex-col gap-2'>
+                            <label className='flex items-center font-montserratMedium text-xs gap-4 text-green-700'>
+                                <FontAwesomeIcon icon={faUser}/>
+                                Nome Completo
+                            </label>
+                            <input
+                                type={"text"}
+                                placeholder={'Digite seu nome completo'}
+                                className='border-green-700 border-2 text-sm px-8 py-2 rounded-xl'
+                                name="username"
+                                value={formik.values.userName}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                            />
+                            {formik.touched.username && formik.errors.username ? (
+                                <div className="text-red-600 text-xs">{formik.errors.username}</div>
+                            ) : null}
+                        </div>
+                        <div className='flex flex-col gap-2'>
+                            <label className='flex items-center font-montserratMedium text-xs gap-4 text-green-700'>
+                                <FontAwesomeIcon icon={faEnvelope}/>
+                                E-mail
+                            </label>
+                            <input
+                                type={"text"}
+                                placeholder={'Digite seu e-mail'}
+                                className='border-green-700 border-2 text-sm px-8 py-2 rounded-xl'
+                                name="email"
+                                value={formik.values.email}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                            />
+                            {formik.touched.email && formik.errors.email ? (
+                                <div className="text-red-600 text-xs">{formik.errors.email}</div>
+                            ) : null}
+                        </div>
+                        <div className='flex flex-col gap-2'>
+                            <label className='flex items-center font-montserratMedium text-xs gap-4 text-green-700'>
+                                <FontAwesomeIcon icon={faLock}/>
+                                Senha
+                            </label>
+                            <input
+                                type={"password"}
+                                placeholder={'Digite sua senha'}
+                                className='border-green-700 border-2 px-8 py-2 text-sm rounded-xl'
+                                name="password"
+                                value={formik.values.password}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                            />
+                            {formik.touched.password && formik.errors.password ? (
+                                <div className="text-red-600 text-xs">{formik.errors.password}</div>
+                            ) : null}
+                        </div>
+                        <div className='mt-5'>
+                            <button
+                                type="submit"
+                                className='text-white px-24 py-2 rounded-xl font-bold text-sm'
+                                style={{backgroundColor: '#FEB132'}}
+                            >
+                                Cadastrar
+                            </button>
+                        </div>
+                        <div className='mt-3 text-xs'>
+                            <p>Já possui conta? <a href={route('login')}
+                                                   className='text-green-700 font-bold cursor-pointer'>Login</a>
+                            </p>
+                        </div>
+                    </form>
                 </div>
-
-                <div className="mt-4">
-                    <InputLabel htmlFor="email" value="Email" />
-
-                    <TextInput
-                        id="email"
-                        type="email"
-                        name="email"
-                        value={data.email}
-                        className="mt-1 block w-full"
-                        autoComplete="username"
-                        onChange={(e) => setData('email', e.target.value)}
-                        required
-                    />
-
-                    <InputError message={errors.email} className="mt-2" />
-                </div>
-
-                <div className="mt-4">
-
-                    <TextInput
-                        id="password"
-                        type="password"
-                        name="password"
-                        value={data.password}
-                        className="mt-1 block w-full"
-                        autoComplete="new-password"
-                        onChange={(e) => setData('password', e.target.value)}
-                        required
-                    />
-
-                    <InputError message={errors.password} className="mt-2" />
-                </div>
-
-                <div className="mt-4">
-                    <InputLabel htmlFor="password_confirmation" value="Confirm Password" />
-
-                    <TextInput
-                        id="password_confirmation"
-                        type="password"
-                        name="password_confirmation"
-                        value={data.password_confirmation}
-                        className="mt-1 block w-full"
-                        autoComplete="new-password"
-                        onChange={(e) => setData('password_confirmation', e.target.value)}
-                        required
-                    />
-
-                    <InputError message={errors.password_confirmation} className="mt-2" />
-                </div>
-
-                <div className="flex items-center justify-end mt-4">
-                    <Link
-                        href={route('login')}
-                        className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    >
-                        Already registered?
-                    </Link>
-
-                    <PrimaryButton className="ms-4" disabled={processing}>
-                        Register
-                    </PrimaryButton>
-                </div>
-            </form>
-        </GuestLayout>
+                <div className={`col-span-2 bg-cover bg-center ${isDesktop ? '' : 'hidden'}`}
+                     style={{backgroundImage: `url(${backgroundImage})`}}></div>
+            </div>
+        </>
     );
 }
